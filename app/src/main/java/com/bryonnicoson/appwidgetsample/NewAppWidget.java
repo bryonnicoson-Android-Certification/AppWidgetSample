9c08c235354db5e915630d29c463899e055705b3
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-14 16:08:36 -0500

    update button

--- app/src/main/java/com/bryonnicoson/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/bryonnicoson/appwidgetsample/NewAppWidget.java
@@ -1,9 +1,12 @@
 package com.bryonnicoson.appwidgetsample;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.RemoteViews;
 
 import java.text.DateFormat;
@@ -38,6 +41,18 @@ public class NewAppWidget extends AppWidgetProvider {
         SharedPreferences.Editor prefEditor = prefs.edit();
         prefEditor.putInt(COUNT_KEY + appWidgetId, count);
         prefEditor.apply();
+
+        // Intent for update button
+        Intent intentUpdate = new Intent(context, NewAppWidget.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        // bundle all the widget IDs as intent extras
+        int[] idArray = new int[]{appWidgetId};
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+        // wrap it as a pending intent for the widget manager to broadcast
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(
+                context, appWidgetId, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT);
+        // set click listener for the button to send the intent
+        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
